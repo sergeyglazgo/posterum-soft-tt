734e3de72487c831cf3d6ef49e99d82a9ef43749
Serhii Haidukov <sergeyglazgo@gmail.com>
2023-06-04 15:52:55 +0300

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/posterum-soft-tt/'
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
